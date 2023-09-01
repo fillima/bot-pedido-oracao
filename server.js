9300963cb272faf75276865ae820dbafb98892ad
Filipe <filipelima849@gmail.com>
2023-09-01 11:14:35 -0300

    Testing new config

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ process.env.NTBA_FIX_319 = 1;
 
 require('dotenv').config();
 
+const PORT = process.env.PORT || 3000;
+
 const TelegramBot = require('node-telegram-bot-api');
 
 const { telegramConfig } = require('./server/configs');
@@ -9,5 +11,9 @@ const { BotController } = require('./server/controllers');
 
 const bot = new TelegramBot(telegramConfig.token, { polling: true });
 const botController = new BotController(bot);
+
+app.listen(PORT, () => {
+    console.log(`Servidor está rodando na porta ${PORT}`);
+});
 
 botController.handle();
